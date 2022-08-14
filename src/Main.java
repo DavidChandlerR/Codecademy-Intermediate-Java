c9ae5e558f0c6ede9293d5cf6ddd7f3806fe7098
David Ryan <David_ryan@live.com>
2022-08-14 12:28:42 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Fuck this shit");
+
+        Person personA = new Person("Dave", 34);
+        Person personB = new Person("Amanda", 31);
+
+        personA.Introduce();
+        personB.Introduce();
+    }
+}
